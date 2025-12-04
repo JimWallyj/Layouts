6b852237d6c950742d546a9fa130eeff899992d3
Jim Walejko <jwalejko@me.com>
2025-12-03 20:38:57 -0700

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Jim Walejko on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
